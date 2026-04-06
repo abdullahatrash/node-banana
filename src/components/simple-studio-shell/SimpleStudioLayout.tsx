254d517a417a1626446e78036162522264b53fe0
abdullahatrash <abodi1987@gmail.com>
2026-04-06 13:37:18 +0300

    feat(simple-studio): add SavePromptDialog with name + preview

--- src/components/simple-studio-shell/SimpleStudioLayout.tsx
+++ src/components/simple-studio-shell/SimpleStudioLayout.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import { useSimpleStudioStore } from "@/store/simpleStudioStore";
 import { SimpleStudioAppSidebar } from "./SimpleStudioAppSidebar";
 import { SimpleStudioSiteHeader } from "./SimpleStudioSiteHeader";
+import { SavePromptDialog } from "./SavePromptDialog";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { modeFromPathname } from "./urlToMode";
 
@@ -45,6 +46,7 @@ export function SimpleStudioLayout({ children }: SimpleStudioLayoutProps) {
       <SidebarInset>
         <SimpleStudioSiteHeader />
         <div className="flex flex-1 flex-col">{children}</div>
+        <SavePromptDialog />
       </SidebarInset>
     </SidebarProvider>
   );
